5bd6dba3478472b7a35bdf00e6e6acaa313e8ab4
João Victor Dutra de Oliveria Borges <borges.jvdo@gmail.com>
2020-06-29 21:42:02 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 //users ip1--20.0.50.51 ip2--192.168.0.5
-export const base_URL_users = 'http://20.0.50.51:3000/auth/'
-export const base_URL_authenticate = 'http://20.0.50.51:3000/auth/authenticate'
-export const base_URL_register = 'http://20.0.50.51:3000/auth/register'
+export const base_URL_users = 'https://api-garnet.herokuapp.com/auth/'
+export const base_URL_authenticate = 'https://api-garnet.herokuapp.com/auth/authenticate'
+export const base_URL_register = 'https://api-garnet.herokuapp.com/auth/register'
 
 //recursos
-export const base_URL_DELETE_PUT_GET_POST_Recursos = 'http://20.0.50.51:3000/recursos/'
+export const base_URL_DELETE_PUT_GET_POST_Recursos = 'https://api-garnet.herokuapp.com/recursos/'
